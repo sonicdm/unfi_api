3e2a3238c24875301842ae9b58bb15e763c556b9
Allan Barcellos <sonicdm@gmail.com>
2020-03-01 11:48:10 -0800

    add individual attribute queries (marketing,ingredients,attributes,nutrition)

--- unfi_api/api/products/products.py
+++ unfi_api/api/products/products.py
@@ -11,10 +11,46 @@ from unfi_api.utils.http import response_to_json
 products_api_endpoint = r'https://products.unfi.com/api/Products/'
 
 
-def get_product_attributes(token, product_id):
-    header = {
-        'authorization': '{token}'.format(token=token, )
+def get_product_attributes_by_product_by_int_id(token, product_int_id):
+    endpoint = "attributes"
+    headers = {
+        "authorization": token
     }
+    product_url = urllib.parse.urljoin('https://products.unfi.com/api/Products/', str(product_int_id))
+    url = urllib.parse.urljoin(product_url, endpoint)
+    response = requests.get(url)
+    return response_to_json(response)
+
+
+def get_product_ingredients_by_product_by_int_id(token, product_int_id):
+    endpoint = "ingredients"
+    headers = {
+        "authorization": token
+    }
+    product_url = urllib.parse.urljoin('https://products.unfi.com/api/Products/', str(product_int_id))
+    url = urllib.parse.urljoin(product_url, endpoint)
+    response = requests.get(url)
+    return response_to_json(response)
+
+
+def get_product_marketing_by_product_by_int_id(token, product_int_id):
+    endpoint = "marketing"
+    headers = {
+        "authorization": token
+    }
+    product_url = urllib.parse.urljoin('https://products.unfi.com/api/Products/', str(product_int_id))
+    url = urllib.parse.urljoin(product_url, endpoint)
+    response = requests.get(url)
+    return response_to_json(response)
+
+
+def get_product_nutrition_by_product_by_int_id(token, product_int_id):
+    headers = {
+        "authorization": token
+    }
+    url = urllib.parse.urljoin('https://products.unfi.com/api/Products/nutrition', str(product_int_id))
+    response = requests.get(url)
+    return response_to_json(response)
 
 
 def get_product_data(token, customer_number, product_code):
@@ -41,3 +77,6 @@ def get_product_by_int_id(token, product_int_id):
     url = urllib.parse.urljoin('https://products.unfi.com/api/Products/', str(product_int_id))
     response = requests.get(url)
     return response_to_json(response)
+
+
+
