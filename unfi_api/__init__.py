6ff6b13d4d9c6498eb0a79c568097b481b057e5f
Allan Barcellos <sonicdm@gmail.com>
2021-10-30 13:59:59 -0700

    begin api client

--- unfi_api/__init__.py
+++ unfi_api/__init__.py
@@ -1,23 +1,57 @@
 # __all__ = ['UnfiAPI', 'product', 'api', 'invoice']
 from unfi_api.api.api import UnfiAPI
+from unfi_api.api.base_classes import APICore
+from unfi_api.api.response import APIResponse, NonJsonResultError
 from unfi_api.search.result import Result
+from unfi_api.product.pricing import Pricing
 
-class UnfiApiClient(UnfiAPI):
-    def __init__(self, user, password, incapusla=True, incapsula_retry=False, incapsula_retry_limit=10):
-        super().__init__(user, password, incapusla, incapsula_retry, incapsula_retry_limit)
 
-    def search(self, query, limit=1000, organic_codes='', attribute_ids='', sales_filters='', brand_ids='', category_ids='', page_number=1):
+class UnfiApiClient:
+    def __init__(self, api: APICore):
+        self.api = api
+
+    def search(
+        self,
+        query,
+        limit=1000,
+        organic_codes="",
+        attribute_ids="",
+        sales_filters="",
+        brand_ids="",
+        category_ids="",
+        page_number=1,
+    ):
         result = self._order_management.brands.get_products_by_full_text(
-            query, limit, organic_codes, attribute_ids, sales_filters, brand_ids, category_ids, page_number
+            query,
+            limit,
+            organic_codes,
+            attribute_ids,
+            sales_filters,
+            brand_ids,
+            category_ids,
+            page_number,
         )
-        res = Result.parse_obj(result['data'])
+        res = Result.parse_obj(result["data"])
+        return res
+
+    def get_pricing(self, product_code: str) -> Pricing:
+
+        response: APIResponse = (
+            self._order_management.brands.get_product_pricing_detail(product_code)
+        )
+        if not response.data:
+            raise NonJsonResultError(
+                f"Non-JSON API response for product code {product_code}"
+            )
+        res = Pricing.parse_obj(response.data)
         return res
 
 
 def main():
-    client = UnfiApiClient("capellaAPI","CapellaAPI2489", incapusla=False)
+    client = UnfiApiClient("capellaAPI", "CapellaAPI2489", incapusla=False)
     result = client.search("chocolate")
     pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
