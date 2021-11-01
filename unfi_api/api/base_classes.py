7d3d28462b43141239c99f31459536a08325ec6c
Allan Barcellos <sonicdm@gmail.com>
2021-11-01 00:10:02 -0700

    base class variables

--- unfi_api/api/base_classes.py
+++ unfi_api/api/base_classes.py
@@ -1,10 +1,19 @@
-class APICore:
-    """Base for API Object"""
-
-    pass
-        
 
 
 
 class Endpoint:
     """Base for API Endpoint"""
+
+
+class APICore:
+    """Base for API Object"""
+
+    ordermanagement: Endpoint
+    products: Endpoint
+    brands: Endpoint
+    categories: Endpoint
+    interactive_reports: Endpoint
+    order_history: Endpoint
+    product_detail: Endpoint
+
+        
